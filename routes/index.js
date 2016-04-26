7719ebb250593bc95e7cd9b430e77cbf6942c91a
Francisco Jordano <arcturus@ardeenelinfierno.com>
2016-04-26 19:50:17 +0100

    First version with oembed (not visually nice)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const sched = require('../lib/sched.js');
+const debug = require('debug')('magnet-adaptor-sched:router');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+function redirectNoSchedUrl(req, res) {
+  res.render('empty', {
+    host: req.headers.host,
+    path: req.originalUrl,
+    protocol: req.secure ? 'https' : 'http',
+  });
+}
+
+router.get('/', (req, res) => {
+  debug(req.query);
+  var schedUrl = req.query.schedUrl;
+  if (!schedUrl) {
+    redirectNoSchedUrl(req, res);
+  } else {
+    schedUrl = schedUrl.replace(/ /g, '+');
+    const parser = new sched.venue(schedUrl);
+    parser.parse()
+      .then((data) => {
+        const transformed = encodeURIComponent(JSON.stringify(data));
+        const oembed = `/oembed/?data=${transformed}`;
+        res.render('index', {
+          data,
+          schedUrl,
+          oembed,
+        });
+      })
+      .catch((err) => {
+        debug('Error parsing ', err);
+        redirectNoSchedUrl(req, res);
+      });
+  }
+});
+
+router.get('/oembed/', (req, res) => {
+  debug('Trying to get oembed data for ', req.query);
+  var data = req.query.data;
+  if (!data) {
+    return res.render('error', {status: 'No data'});
+  }
+  try {
+    data = JSON.parse(decodeURIComponent(data));
+  } catch(err) {
+    return res.render('error', err);
+  }
+  debug('Got data ', data);
+  res.render('oembed', data);
 });
 
 module.exports = router;
