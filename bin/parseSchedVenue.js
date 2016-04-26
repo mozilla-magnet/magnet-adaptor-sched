237fd05945cbdf4cc1c797312b49042dfb281276
Francisco Jordano <arcturus@ardeenelinfierno.com>
2016-04-26 18:26:23 +0100

    Lib for parsing events (scrapping)

---
+++ bin/parseSchedVenue.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2));
+
+const usage = 'Use: parseSchedVenue --url=<url>';
+
+if(!argv['url']) {
+  console.log(usage);
+  process.exit(1);
+}
+
+const url = argv['url'];
+const sched = require('../lib/sched.js');
+
+const parser = new sched.venue(url);
+
+parser.parse()
+  .then((info) => {
+    console.log('Information parsed:');
+    console.log(info);
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error('Error parsing: ', err);
+    process.exit(2);
+  });
