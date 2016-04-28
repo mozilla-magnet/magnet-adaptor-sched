215aae66de6409388f1072509f3d4f05a230d5b5
Wilson Page <wilsonpage@me.com>
2016-04-28 13:30:24 +0100

    Added styling

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+'use strict';
+
+/**
+ * Dependencies
+ */
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
 
 var app = express();
 
@@ -13,15 +17,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
+
+/**
+ * Error Handlers
+ */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,8 +34,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handlers
 
 // development error handler
 // will print stacktrace
@@ -54,5 +57,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+/**
+ * Exports
+ */
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,64 @@
+
+/**
+ * Dependencies
+ */
+
+const debug = require('debug')('magnet-adaptor-sched:router');
+const sched = require('../lib/sched.js');
 const express = require('express');
 const router = express.Router();
-const sched = require('../lib/sched.js');
-const debug = require('debug')('magnet-adaptor-sched:router');
+
+router.get('/', (req, res) => {
+  debug(req.query);
+  var schedUrl = req.query.url;
+  if (!schedUrl) return redirectNoSchedUrl(req, res);
+
+  schedUrl = schedUrl.replace(/ /g, '+');
+  const parser = new sched.Venue(schedUrl);
+
+  parser.parse()
+    .then((data) => {
+      const transformed = encodeURIComponent(JSON.stringify(data));
+      const oembed = `/oembed/?data=${transformed}`;
+      res.render('index', {
+        data,
+        schedUrl,
+        oembed
+      });
+    })
+
+    .catch((err) => {
+      debug('Error parsing ', err);
+      redirectNoSchedUrl(req, res);
+    });
+});
+
+router.get('/oembed/', (req, res) => {
+  debug('Trying to get oembed data for ', req.query);
+
+  var data = req.query.data;
+  if (!data) return res.render('error', {message: 'No data'});
+
+  try {
+    data = JSON.parse(decodeURIComponent(data));
+  } catch (err) {
+    return res.render('error', err);
+  }
+
+  debug('got data ', data);
+  res.render('oembed', data);
+});
 
 function redirectNoSchedUrl(req, res) {
   res.render('empty', {
     host: req.headers.host,
     path: req.originalUrl,
-    protocol: req.secure ? 'https' : 'http',
+    protocol: req.secure ? 'https' : 'http'
   });
 }
 
-router.get('/', (req, res) => {
-  debug(req.query);
-  var schedUrl = req.query.url;
-  if (!schedUrl) {
-    redirectNoSchedUrl(req, res);
-  } else {
-    schedUrl = schedUrl.replace(/ /g, '+');
-    const parser = new sched.venue(schedUrl);
-    parser.parse()
-      .then((data) => {
-        const transformed = encodeURIComponent(JSON.stringify(data));
-        const oembed = `/oembed/?data=${transformed}`;
-        res.render('index', {
-          data,
-          schedUrl,
-          oembed,
-        });
-      })
-      .catch((err) => {
-        debug('Error parsing ', err);
-        redirectNoSchedUrl(req, res);
-      });
-  }
-});
-
-router.get('/oembed/', (req, res) => {
-  debug('Trying to get oembed data for ', req.query);
-  var data = req.query.data;
-  if (!data) {
-    return res.render('error', {status: 'No data'});
-  }
-  try {
-    data = JSON.parse(decodeURIComponent(data));
-  } catch(err) {
-    return res.render('error', err);
-  }
-  debug('Got data ', data);
-  res.render('oembed', data);
-});
+/**
+ * Exports
+ */
 
 module.exports = router;
